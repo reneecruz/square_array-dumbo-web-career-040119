2d7ccf3d3dbdf0b481021a28cf506d0ce4a9f1f1
Renée Cruz <reneebasscruz@gmail.com>
2019-03-21 01:37:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,4 @@
 #def square_array(numbers)
-  #new_numbers = []
-  #new_numbers = numbers.each do |i| 
-  #puts i**2 
-  #return new_numbers
-  #end
-  # your code here
-  #return new_array
 def square_array(array)
   new_array = []
   array.each do |element|
