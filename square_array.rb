b35fad60b98e00639a2079779c20ab3e54e0863d
Renée Cruz <reneebasscruz@gmail.com>
2019-03-21 01:23:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,8 @@ def square_array(numbers)
   #numbers.each do |i| 
   #numbers.each do  |number| 
   #puts number**2 
-  numbers.each do |i| puts i**2 
+  numbers.each do |i| 
+  puts i**2 
   end
   # your code here
   #numbers.each do |i|
