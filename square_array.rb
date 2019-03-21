631c851b11b651493181250afde17e01753c03a3
Renée Cruz <reneebasscruz@gmail.com>
2019-03-21 00:49:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |i| 
+  Integer.sqrt(i)
+  end
   # your code here
+  #numbers.each do |i|
+  #return new_array
 end
